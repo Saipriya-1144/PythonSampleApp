5ba89a8f13822d881be6e67cb6f497c6ea6d97d2
Saipriya-1144 <saipriyach1144@gmail.com>
2024-09-30 09:28:46 +0530

    modified app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def dummy():
 @app.route("/placeholder")
 def placeholder():
     print("Placeholder endpoint called!")
-    return "f045e2a8-0018-409e-b59f-245d94ec22b5"
+    return "<<<py-place-holder>>>"
